2a9b32ef1b2b26cdad9a43f73818102fdc50eb7a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-28 20:48:38 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
-import React from "react";
+import io from "socket.io-client";
 
 export default function App() {
+  const [socket, setSocket] = useState(null);
+  const [welcome, setWelcome] = useState(true);
+  const [room, setRoom] = useState(false);
+  const [roomName, setRoomName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [usersCount, setUsersCount] = useState(0);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+    setSocket(newSocket);
+
+    return () => newSocket.close();
+  }, []);
+
+  const handleWelcomeSubmit = (event) => {
+    event.preventDefault();
+    socket.emit("enter_room", roomName, () => {
+      setWelcome(false);
+      setRoom(true);
+    });
+  };
+
   return (
     <div className="container">
       <div className="header">
         <div className="header-box">
           <img src="images/profile.png" className="img" alt="" />
-          <div className="welcome">
-            <form>
+          {welcome && (
+            <form onSubmit={handleWelcomeSubmit}>
               <input
                 type="text"
                 placeholder="방 제목"
                 required
+                value={roomName}
                 className="room-name"
               />
             </form>
-          </div>
+          )}
+          {room && (
+            <div>
+              <h2>{roomName}</h2>
+            </div>
+          )}
         </div>
       </div>
     </div>
