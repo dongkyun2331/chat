993ed25f15ddcb20b994d349920ab3696e9dd03c
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-28 20:05:47 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,16 @@ export default function App() {
       <div className="header">
         <div className="header-box">
           <img src="images/profile.png" className="img" alt="" />
+          <div className="welcome">
+            <form>
+              <input
+                type="text"
+                placeholder="방 제목"
+                required
+                className="room-name"
+              />
+            </form>
+          </div>
         </div>
       </div>
     </div>
