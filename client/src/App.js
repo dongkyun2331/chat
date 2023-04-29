34b70df627f80b395d62ffb0a87b246fbb30fd1a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-29 14:50:01 +0900

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 import Join from "./components/Join";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" component={Join} />
-      <Route path="/chat" component={Chat} />
+      <Routes>
+        <Route path="/" component={Join} />
+        <Route path="/chat" component={Chat} />
+      </Routes>
     </Router>
   );
 };
